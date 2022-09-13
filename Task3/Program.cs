0847c2d69647f83b1b8277cdd1600f724411c49c
«Antonina <a1523@mail.ru>
2022-09-13 23:33:15 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -9,23 +9,32 @@ int[] InputArray ()
     int[] array = new int[8];
     for(int i = 0; i < array.Length; i++)
     {
-        Console.Write($"Введите элемент массива {i}: ");
-        array[i] = Convert.ToInt32(Console.ReadLine());
+        try
+        {
+            Console.Write($"Введите элемент массива с индексом {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка ввода элементов массива! Введите целое число.");
+            i--;
+        }
     }
     return array;
 }
 
-    void PrintArrayToConsole(int[] array)
+void PrintArrayToConsole(int[] array)
 {
     Console.Write("Массив из 8 элементов: ");
     for(int i = 0; i < array.Length-1; i++)
     {
         Console.Write($"{array[i]}, ");
     }
-       for(int i = array.Length-1; i < array.Length; i++)
+    for(int i = array.Length-1; i < array.Length; i++)
     {
         Console.Write($"{array[i]}");
     } 
+    
 }
 
 int[] array = InputArray();
