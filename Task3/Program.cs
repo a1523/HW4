3f0dba84239bcc212d2882ac7ffbc7e474d7e5de
«Antonina <a1523@mail.ru>
2022-09-13 23:22:52 +0300

    Add gitignore

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся 
+// пользователем.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] InputArray ()
+{
+    int[] array = new int[8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+    void PrintArrayToConsole(int[] array)
+{
+    Console.Write("Массив из 8 элементов: ");
+    for(int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+       for(int i = array.Length-1; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    } 
+}
+
+int[] array = InputArray();
+PrintArrayToConsole(array);
